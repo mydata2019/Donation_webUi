5783b1dcf5d2aa32fc1114b57dfa2500f53e9416
mydata2019 <55942385+mydata2019@users.noreply.github.com>
2019-10-11 22:04:24 +0900

    Add files via upload

--- js/linkMain.js
+++ js/linkMain.js
@@ -9,13 +9,15 @@ $(document).ready(function(){
 
 function to_back(){
   console.log("back");
-  location.href="./mainMyDon.jsp?userId="+userId;
+  // location.href="./mainMyDon.jsp?userId="+userId;
+  location.href="./mainMyDon.jsp?userId=1";
 }
 
 //페이지 이동
 function layerOpen(id) {
     console.log("id is "+id);
-    location.href="./"+id+".jsp?userId="+userId;
+    // location.href="./"+id+".jsp?userId="+userId;
+    location.href="./"+id+".jsp?userId=1";
 
 //    location.href='./linkByAPI.jsp?user_id=1&org_id=1'
 };
